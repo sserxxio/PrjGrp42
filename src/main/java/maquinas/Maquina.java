fecbc284560866c9b4eb3771e77dc4a4d68bebab
Pascual El Mago <pascualrosonsoto@gmail.com>
2026-04-23 20:27:38 +0200

    Issues 7 y 8

--- src/main/java/maquinas/Maquina.java
+++ src/main/java/maquinas/Maquina.java
@@ -1,16 +1,24 @@
 package maquinas;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Maquina {
 	private int id;
-	private ArrayList<Integer> coordenadas;
+	private float coordenadas[];
 	private int capacidad;
-	private Boolean estaOperativa;
+	private int slots;
+	private HashMap<String, Integer> inventario = new HashMap<>();
+	private ArrayList<Producto> productos = new ArrayList<>();
+	private boolean estaOperativa;
 	
-	public Maquina(int id, ArrayList<Integer> coordenadas, int capacidad, Boolean estaOperativa) {
+	public Maquina(int id, float coordenadaX, float coordenadaY, int capacidad, int slots, boolean estaOperativa) {
 		this.id=id;
-		this.coordenadas=coordenadas;
+		this.coordenadas= new float[2];
+		this.coordenadas[0] = coordenadaX;
+		this.coordenadas[1] = coordenadaY;
 		this.capacidad=capacidad;
+		this.slots = slots;
 		this.estaOperativa=estaOperativa;
 	}
 
@@ -20,7 +28,7 @@ public class Maquina {
 
     public void informacion(){
         System.out.println("\nID: " + id + 
-        		"\nCoordenadas: " + coordenadas.get(0) + ", " + coordenadas.get(1) + 
+        		"\nCoordenadas: " + coordenadas[0] + ", " + coordenadas[1] + 
         		"\nCapacidad: " + capacidad +
         		"\nOperativa: " + estaOperativa);
     }
@@ -39,7 +47,17 @@ public class Maquina {
     public void notificarVenta(Producto producto,int cantidad) {
     	System.out.println("Se han vendido "+ cantidad +" del producto"+producto.getNombre());
     }
-    
+    public void inicializarStock() {
+    	Scanner sc = new Scanner(System.in);
+    	for(int i =0; i<this.slots; i++) {
+    		System.out.println("Producto "+i);
+    		System.out.print("Nombre: ");
+    		String linea = sc.nextLine().trim();
+    		this.productos.add(new Producto("botella", linea));
+    		this.inventario.put(linea, this.capacidad);
+    	}
+    	//sc.close();
+    }
     
 
 }
