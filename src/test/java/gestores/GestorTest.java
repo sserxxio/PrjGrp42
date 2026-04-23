abe8340f3d21f0a59feaf122421c18ff2667c1ca
Serxio <sergiosoutomourelle@gmail.com>
2026-04-23 12:18:02 +0200

    Creados tests de ver maquinas

--- src/test/java/gestores/GestorTest.java
+++ src/test/java/gestores/GestorTest.java
@@ -4,6 +4,8 @@ import maquinas.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.DisplayName;
@@ -27,4 +29,43 @@ class GestorTest {
 
         assertNotNull(maquina, "La máquina no debería ser null");
     }
+    
+    @DisplayName("Prueba de Gestor: Ver maquina")
+    @Test
+    void testVerMaquina() {
+
+    	Gestor gestor = new Gestor();
+
+        ArrayList<Integer> coord = new ArrayList<>();
+        coord.add(2);
+        coord.add(3);
+
+        gestor.registrarMaquina(1, coord, 4, true);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        gestor.mostrarInformacion(1);
+
+        String salida = outContent.toString();
+
+        assertTrue(salida.contains("Máquina encontrada"));
+    }
+    
+    @DisplayName("Prueba de Gestor: Ver maquina falla")
+    @Test
+    void testVerMaquinaFalla() {
+
+        Gestor gestor = new Gestor();
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        gestor.mostrarInformacion(999);
+
+        String salida = outContent.toString();
+
+        assertTrue(salida.contains("Error: no existe una máquina"));
+    }
+    
 }
