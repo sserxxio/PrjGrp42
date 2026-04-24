407df3757a82fcafa5084c58e900fd5e9955fe10
Pascual El Mago <pascualrosonsoto@gmail.com>
2026-04-24 12:32:38 +0200

    Issue#6

--- src/main/java/gestores/Gestor.java
+++ src/main/java/gestores/Gestor.java
@@ -1,6 +1,8 @@
 package gestores;
 
 import maquinas.Maquina;
+import maquinas.Producto;
+import maquinas.Estimacion;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -41,5 +43,12 @@ public class Gestor {
             System.out.println("Máquina encontrada:");
             m.informacion();
         }
+    }
+    public boolean notificarReposicion(Maquina m, Producto p) {
+    	if(Estimacion.calcularEstimacion(m, p) <2) {
+    		return true;
+    	}else {
+    		return false;
+    	}
     }
 }
